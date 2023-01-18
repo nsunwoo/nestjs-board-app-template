f27f2b2af3802b0b115af7c83b58795141138ba5
nsunwoo <sunwoo.stats@gmail.com>
2023-01-19 01:19:24 +0900

    feat: Add sign-up feature

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,14 @@
 import { CustomRepository } from '../typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}
