b4c0abf3b9183ec6b044f5b17898bf7e784bb304
nsunwoo <sunwoo.stats@gmail.com>
2023-01-19 02:13:09 +0900

    feat: Add feature for unique username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,10 @@ import { CustomRepository } from '../typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,6 +13,14 @@ export class UserRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
