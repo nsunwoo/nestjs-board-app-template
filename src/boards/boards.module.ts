13387864d48ff471153e4434e36850ef323bdfa1
nsunwoo <sunwoo.stats@gmail.com>
2023-01-26 14:51:12 +0900

    feat: Let only verified users control boards

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,10 @@ import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { BoardRepository } from './board.repository';
 import { TypeOrmExModule } from '../typeorm-ex.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([BoardRepository]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
