b758b5ba98448af63036ce125976c9d12b6ea5ef
nsunwoo <sunwoo.stats@gmail.com>
2023-01-26 16:13:26 +0900

    feat: Add 'user' parameter to get all boards posted by certain user

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
-import { v1 as uuid } from 'uuid';
+// import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
@@ -15,8 +15,14 @@ export class BoardsService {
   //   return this.boards;
   // }
 
-  async getAllBoards(): Promise<Board[]> {
-    return this.boardRepository.find();
+  async getAllBoards(user: User): Promise<Board[]> {
+    const query = this.boardRepository.createQueryBuilder('board');
+
+    query.where('board.userId = :userId', { userId: user.id });
+
+    const boards = await query.getMany();
+
+    return boards;
   }
 
   // createBoard(createBoardDto: CreateBoardDto) {
